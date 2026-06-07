c42d02f489dea90761051747294263e356d36a28
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2026-06-07 14:29:17 +0300

    alternate

--- src/MediaPR/BlogsPage.jsx
+++ src/MediaPR/BlogsPage.jsx
@@ -10,49 +10,85 @@ export default function BlogsPage() {
   const navigate = useNavigate();
 
   return (
-    <section className="bg-[#f5f5f3] min-h-screen px-6 md:px-16 py-20">
+    <section className="bg-black min-h-screen px-6 md:px-16 py-20">
         <Navbar/>
       <h1 className="text-5xl font-serif text-[#9b6b4d] mb-20">
         Media & PR
       </h1>
 
       <div className="space-y-24">
-        {blogs.map((blog) => (
+        {blogs.map((blog , index) => (
           <div
-            key={blog.id}
-            className="grid grid-cols-1 md:grid-cols-2 gap-14 items-center"
-          >
-            {/* IMAGE */}
-            <div>
-              <img
-                src={blog.image}
-                alt={blog.title}
-                className="w-full h-105 object-cover"
-              />
-            </div>
+  key={blog.id}
+  className="grid grid-cols-1 md:grid-cols-2 gap-16 items-center"
+>
+          {index % 2 === 0 ? (
+  <>
+    {/* IMAGE */}
+    <div className="overflow-hidden">
+      <img
+        src={blog.image}
+        alt={blog.title}
+        className="w-full h-[500px] object-cover transition duration-700 hover:scale-105"
+      />
+    </div>
 
-            {/* CONTENT */}
-            <div>
-              <p className="uppercase text-gray-500 tracking-wide text-sm mb-4">
-                {blog.date}
-              </p>
+    {/* TEXT */}
+    <div>
+      <p className="uppercase text-gray-200 tracking-[0.2em] text-sm mb-4">
+        {blog.date}
+      </p>
 
-              <h2 className="text-4xl font-serif text-[#9b6b4d] leading-tight">
-                {blog.title}
-              </h2>
+      <h2 className="text-4xl md:text-5xl font-serif text-[#9b6b4d] leading-tight">
+        {blog.title}
+      </h2>
 
-              <p className="mt-7 text-xl leading-10 text-[#222]">
-                {blog.short}
-              </p>
+      <p className="mt-8 text-lg leading-9 text-white">
+        {blog.full}
+      </p>
 
-              {/* OPEN PARTICULAR BLOG */}
-              <button
-                onClick={() => navigate(`/media-pr/${blog.id}`)}
-                className="mt-8 border-b border-black text-xl"
-              >
-                Read more
-              </button>
-            </div>
+      <button
+        onClick={() => navigate(`/media-pr/${blog.id}`)}
+        className="mt-8 inline-flex items-center border-b border-black pb-1 text-sm text-gray-300 transition"
+      >
+        Read More
+      </button>
+    </div>
+  </>
+) : (
+  <>
+    {/* TEXT */}
+    <div className="order-2 md:order-1">
+      <p className="uppercase text-gray-500 tracking-[0.2em] text-sm mb-4">
+        {blog.date}
+      </p>
+
+      <h2 className="text-4xl md:text-5xl font-serif text-[#9b6b4d] leading-tight">
+        {blog.title}
+      </h2>
+
+      <p className="mt-8 text-lg leading-9 text-white">
+        {blog.full}
+      </p>
+
+      <button
+        onClick={() => navigate(`/media-pr/${blog.id}`)}
+        className="mt-8 inline-flex items-center border-b border-black pb-1text-sm text-gray-300 transition"
+      >
+        Read More
+      </button>
+    </div>
+
+    {/* IMAGE */}
+    <div className="order-1 md:order-2 overflow-hidden">
+      <img
+        src={blog.image}
+        alt={blog.title}
+        className="w-full h-[500px] object-cover transition duration-700 hover:scale-105"
+      />
+    </div>
+  </>
+)}
           </div>
         ))}
       </div>
