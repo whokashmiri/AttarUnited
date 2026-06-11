a8eca69efb863fee58d8375112eb4906c56e740c
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2026-06-11 09:51:09 +0300

    IT banner

--- src/ITTeamRibbon.jsx
+++ src/ITTeamRibbon.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function ITTeamRibbon() {
   return (
-    <div className=" fixed top-[90%] z-999 w-full bg-[#9b6b4d] text-white text-center py-2 text-sm md:text-base font-medium tracking-wide">
+    <div className=" fixed top-[90%] z-999 w-full bg-[#30221a] text-white text-center py-2 text-sm md:text-base font-medium tracking-wide">
       Made by IT Team
     </div>
   );
