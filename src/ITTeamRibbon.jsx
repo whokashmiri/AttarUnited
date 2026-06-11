fdcfbc3870b0118fede8a5f9c178e58fcbd25a1f
You-sef123 <jokkeerr@gmail.com>
2026-06-11 14:01:31 +0300

    last update

--- src/ITTeamRibbon.jsx
+++ src/ITTeamRibbon.jsx
@@ -4,8 +4,8 @@ import React from "react";
 
 export default function ITTeamRibbon() {
   return (
-    <div className=" fixed top-[90%] z-999 w-full bg-[#30221a] text-white text-center py-2 text-sm md:text-base font-medium tracking-wide">
-      Made by IT Team
+    <div className="fixed top-[90%] z-[999] w-full bg-[#30221a] text-white text-center py-2 text-sm md:text-base font-medium tracking-wide">
+      This website is currently under development and has not been officially launched. For testing purposes only. Made by IT team , AttarUnited
     </div>
   );
 }
